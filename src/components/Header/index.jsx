b33f1d2418bb9f823975f0a5060b7907f9485c54
Abu Sufyan Abud <>
2024-10-03 15:28:21 +0500

    Update index.jsx
    list-change

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -109,7 +109,7 @@ function Header() {
                 </Link>
               </li> */}
 
-              <li className="relative group py-2">
+              <li className=" group py-2">
                 <Link
                   href="/"
                   className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-white-400 lg:dark:hover:text-white dark:hover:text-white lg:dark:hover:bg-transparent"
