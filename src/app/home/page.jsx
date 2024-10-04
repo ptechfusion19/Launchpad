67ae62f6d7c3d0e6cbd7282d52c28151b3bf4a73
MrSohaibAhmed <129491757+MrSohaibAhmed@users.noreply.github.com>
2024-10-04 16:24:59 +0500

    updated

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,58 +1,84 @@
-"use client";
+"use client"
 import { Toaster } from "react-hot-toast";
 import Header from "@/components/Header";
+import CardComp from "@/components/CardComp";
 import NewCardComp from "@/components/NewCardComp";
-import Image from "next/image"; // Ensure you import Image
-import logo from "../../../public/logo.png";
 
 const LaunchPage = () => {
     return (
         <>
             <Toaster position="top-center" reverseOrder={false} />
             <Header />
-            <div className="py-20 h-4/5  flex items-center justify-center ">
-                <NewCardComp className="">
-                    {/* Left side for text content */}
-                    <div className="w-3/5 p-4 text-white">
-                        <h3 className="text-white text-center pb-2 mb-4 text-xl">Welcome to Bundle Bee – Your Solana Meme Launchpad</h3>
-                        <h4 className="text-white font-medium mb-4">
+            <div className="py-20 h-fit flex items-center">
+                <NewCardComp>
+                    <div className="px-2 text-center">
+                        <h3 className="text-center pb-2 mb-4 text-2xl">
+                            Welcome to Bundle Bee – Your Solana Meme Launchpad
+                        </h3>
+                        <h4 className="font-medium">
                             Tired of the same old launchpad routines? Say hello to Bundle Bee, where innovation meets opportunity.
                             <br />
                             We're not just another launchpad; we're your strategic partner in the bustling world of Solana Meme projects.
                         </h4>
-                        <br/>
-                        <h3 className="pb-2 mb-4 text-xl text-white">Why Bundle Bee?</h3>
-                        <ul className="list-disc list-inside text-left text-base font-medium space-y-2">
-                            <li><strong>Strategic Launches:</strong> We bundle launches to secure better positioning for developers, steering clear of snipers.</li>
-                            <li><strong>Cost-Effective:</strong> We pride ourselves on being the most cost-effective launchpad on the market.</li>
-                            <li><strong>Beyond the Pump:</strong> Bundle Bee is committed to fostering sustainable projects, not transient hype.</li>
-                            <li><strong>Support for Creators:</strong> We're here to support creators in making each launch count.</li>
+                        <br />
+                        <h3 className="text-center pb-2 mb-4 text-2xl">
+                            Why Bundle Bee?
+                        </h3>
+
+                        <ul className="list-disc list-inside text-left mx-auto w-2/3 text-lg font-medium">
+                            <li className="bg-gray-800 text-white p-4 rounded-lg shadow-md mb-2 hover:bg-gray-700 transition duration-300">
+                                <strong>Strategic Launches</strong>: We bundle launches to secure better positioning for developers, steering clear of snipers. It's about strategic placement, not just speed.
+                            </li>
+                            <li className="bg-gray-800 text-white p-4 rounded-lg shadow-md mb-2 hover:bg-gray-700 transition duration-300">
+                                <strong>Cost-Effective</strong>: We pride ourselves on being the most cost-effective launchpad on the market. With JITO, we empower teams to keep more of what they earn.
+                            </li>
+                            <li className="bg-gray-800 text-white p-4 rounded-lg shadow-md mb-2 hover:bg-gray-700 transition duration-300">
+                                <strong>Beyond the Pump</strong>: Tired of the 'pump and dump' culture? We are too. Bundle Bee is committed to fostering sustainable projects, not transient hype.
+                            </li>
+                            <li className="bg-gray-800 text-white p-4 rounded-lg shadow-md mb-2 hover:bg-gray-700 transition duration-300">
+                                <strong>Support for Creators</strong>: Launching tokens isn't cheap, and not every project hits the jackpot. We're here to support creators in making each launch count.
+                            </li>
                         </ul>
-                        <br/>
-                        <h3 className="pb-2 mb-4 text-xl text-white">What We Offer</h3>
-                        <ul className="list-disc list-inside text-left text-base font-medium space-y-2">
-                            <li><strong>For Developers:</strong> A robust platform to launch with confidence.</li>
-                            <li><strong>For Users:</strong> Tools tailored for Solana users.</li>
-                            <li><strong>Revenue Reinvestment:</strong> Ensuring growth for both Bundle Bee and its community.</li>
-                        </ul>
-                        <br/>
-                        <h3 className="pb-2 mb-4 text-xl text-white">Upcoming Projects:</h3>
-                        <ul className="list-disc list-inside text-left">
-                            <li><strong>BundleBee Telegram APP:</strong> Launching soon after our Solana debut.</li>
-                        </ul>
-                        <br/>
-                        <h3 className="text-white pb-2 mb-4 text-xl">Join the Hive:</h3>
-                        <ul className="list-disc list-inside text-left">
-                            <li>Whether you're a developer looking to launch your next big thing or a user eager to explore new tools, Bundle Bee is where you belong. Let's build, launch, and succeed together on Solana!
+                        <br />
+                         <h3 className="text-center pb-2 mb-4 text-2xl">
+                            What We Offer
+                        </h3>
+                        <ul className="list-disc list-inside text-left mx-auto w-2/3 text-lg font-medium space-y-2">
+                            <li>
+                                <strong className="text-blue-600">For Developers:</strong> A robust platform to launch with confidence. We provide tools and support to ensure your project stands out and succeeds.
+                            </li>
+                            <li>
+                                <strong className="text-blue-600">For Users:</strong> More than just a launchpad, we're rolling out tools tailored for Solana users, enhancing your interaction with the ecosystem.
+                            </li>
+                            <li>
+                                <strong className="text-blue-600">Revenue Reinvestment:</strong> We use platform revenue for buybacks and marketing, ensuring growth for both Bundle Bee and its community.
                             </li>
                         </ul>
 
-                        <h4 className="py-7 text-lg font-medium text-center text-white">Get Started Today and Be Part of the Revolution!</h4>
-                    </div>
+                        <br />
+                        <h3 className="text-center pb-2 mb-4 text-2xl">
+                            Upcoming Projects:
+                        </h3>
+                        <ul>
+                            <li>
+                                <strong>BundleBee Telegram APP</strong>:  Launching soon after our Solana debut. Stay tuned for a new way to engage with your favorite projects right from your Telegram.
+                            </li>
+                        </ul>
+                        <br />
+                        <h3 className="text-center pb-2 mb-4 text-2xl">
+                            Join the Hive:
+                        </h3>
+                        <ul>
+                            <li>
+                                Whether you're a developer looking to launch your next big thing or a user eager to explore new tools, Bundle Bee is where you belong. Let's build, launch, and succeed together on Solana!
+                            </li>
+                        </ul>
 
-                    {/* Right side for the logo */}
-                    <div className="w-2/5 flex items-start justify-center">
-                        <Image className="mt-5" src={logo} alt="Logo" height={300} width={300} />
+
+
+                        <h4 className="py-7 text-lg font-medium">
+                            Get Started Today and Be Part of the Revolution!
+                        </h4>
                     </div>
                 </NewCardComp>
             </div>
--- src/components/NewCardComp/index.jsx
+++ src/components/NewCardComp/index.jsx
@@ -3,11 +3,32 @@ import React from "react";
 
 const NewCardComp = ({ children }) => {
   return (
-    <div className="w-4/6 bg-[#141212] border border-custom_bg rounded-xl shadow sm:p-3 md:p-3 dark:border-gray-700">
-      <div className="text-white flex justify-around">
+    // <div
+    //   style={{
+    //     width: "80%",
+    //     padding: "1rem",
+    //     margin: "0 auto",
+    //     backgroundColor: "#383962",
+    //     border: "1px solid #383962",
+    //     borderRadius: "0.75rem",
+    //     boxShadow: "0 1px 2px rgba(0, 0, 0, 0.05)",
+    //     display: "flex",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     flexDirection : "column"
+    //   }}
+    // >
+    //   {children}
+    // </div>
+    // <div className="">
+
+    <div className=" text-center w-4/5 p-4 mx-auto bg-[#F7AA00] border border-custom_bg rounded-xl shadow sm:p-6 md:p-8  dark:border-gray-700">
+      <div className="flex items-center justify-center flex-col">
         {children}
       </div>
     </div>
+
+    // </div>
   );
 };
 
